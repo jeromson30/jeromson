69d9f7edc10a5655669f5f440d51e68916bfca24
jeromson30 <jerome.corso@gmail.com>
2024-07-21 19:19:42 +0200

    style: base CSS

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,20 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+       <h1>Jérôme CORSO<sup>Web Developper</sup></h1>
+       <nav id="menu">
+          <a>Accueil</a>
+          <a>Compétences</a>
+          <a>Projets</a>
+       </nav>
+      </header>
+      <main>
+      <h2>Under construction</h2>
+      </main>
+      <footer>
+
+      </footer>
     </>
   )
 }
