59275948763fd258bbf73b5d34ad82028fd38569
jeromson30 <jerome.corso@gmail.com>
2024-08-04 19:03:02 +0200

    style: add icons contact and top bar effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
 import photoProfil from './assets/profil.jpg'
-import viteLogo from '/vite.svg'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareEnvelope, faJ } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faSquareInstagram, faSquareGithub } from '@fortawesome/free-brands-svg-icons'
+
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +13,11 @@ function App() {
   return (
     <>
       <header>
-       <nav id="menu">
+      <div className="lines">
+        <div className="line"></div>
+      </div>
+      <div className='logo'><FontAwesomeIcon icon={faJ} /></div>
+       <nav className="menu">
           <a>Accueil</a>
           <a>Compétences</a>
           <a>Projets</a>
@@ -21,8 +29,17 @@ function App() {
         <h1>Jérôme CORSO</h1>
         <img src={photoProfil} className='photoProfil' />
         <sup>Web Developper</sup>
+        <div className="contactIcons">
+        <FontAwesomeIcon icon={faSquareEnvelope} />
+        <FontAwesomeIcon icon={faLinkedin} />
+        <FontAwesomeIcon icon={faSquareGithub} />
+        <FontAwesomeIcon icon={faSquareInstagram} />
+        </div>
       </div>
-      <h2>Under construction</h2>
+      <section>
+        <h2>Under construction</h2>
+      </section>
+      
       </main>
       <footer>
 
